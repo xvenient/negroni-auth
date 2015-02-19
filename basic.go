1d3ff36daf0c954bb71db19e465c01170cf44bf7
An Phung <an.phung@hde.co.jp>
2015-02-19 07:16:12 +0000

    Move auth.NewSimpleBasic from datastore.go to basic.go

--- basic.go
+++ basic.go
@@ -20,6 +20,19 @@ const (
 	bcryptCost             = 12
 )
 
+// NewSimpleBasic returns *datastore.Simple built from userid, password.
+func NewSimpleBasic(userId, password string) (*datastore.Simple, error) {
+	hashedPassword, err := Hash(password)
+	if err != nil {
+		return nil, err
+	}
+
+	return &datastore.Simple{
+		Key:   userId,
+		Value: hashedPassword,
+	}, nil
+}
+
 // requireAuth writes error to client which initiates the authentication process
 // or requires reauthentication.
 func requireAuth(w http.ResponseWriter) {
